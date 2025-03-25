a4b7899ab69762c7e9f9b0f8e32a2d818a10820f
artimonist <artimonist@gmail.com>
2025-03-25 13:16:23 +0800

    push temp

--- src/bits/offset.rs
+++ src/bits/offset.rs
@@ -6,7 +6,35 @@ impl<const N: usize> std::ops::Shl<usize> for Bits<N> {
 
     fn shl(self, n: usize) -> Self::Output {
         assert_overflow!(n, 1, N * 8 - 1, "<<");
-        todo!()
+        if n % 8 == 0 {
+            let mut data = self.0;
+            data[n / 8..].copy_from_slice(&self.0[..N - n / 8]);
+            Bits(data)
+        } else if n < 8 {
+            let mut data = vec![0; N];
+            let mut carry = 0;
+            for i in 0..N {
+                let next_carry = self.0[i] >> (8 - n);
+                data[i] = (self.0[i] << n) | carry;
+                carry = next_carry;
+            }
+            Bits(data.try_into().unwrap())
+        } else {
+            todo!()
+        }
+
+        // let mut data = vec![0; N];
+        // if n >= 8 {
+        //     self.0[..N - n.div_ceil(8)];
+        // }
+        // let mut data = self.0[..N - n / 8].to_vec();
+        // let mut carry = 0;
+        // for i in 0..N - n / 8 {
+        //     let next_carry = self.0[i] >> (8 - n % 8);
+        //     data[i] = (self.0[i] << n % 8) | carry;
+        //     carry = next_carry;
+        // }
+        // Bits(data.try_into().unwrap())
     }
 }
 
