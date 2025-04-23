bb51d0eed6c7e09be9ba9607d3b615c5dcfb700a
artimonist <artimonist@gmail.com>
2025-04-23 17:22:17 +0000

    impl xbits::bit_chunks

--- src/xbits/iter.rs
+++ src/xbits/iter.rs
@@ -1,3 +1,5 @@
+use crate::assert_overflow;
+
 /**
  * Bits iterator on [u8]
  */
@@ -23,11 +25,30 @@ pub trait BitIterator {
     /// ```
     fn bit_from_iter(&mut self, iter: impl Iterator<Item = bool>);
 
+    /// Returns the bits in the buffer grouped by n  
+    ///
+    /// # Parameters  
+    /// - `T`: the type to contains the grouped bits  
+    /// - `n`: the number of bits to group  
+    /// - 1 <= n <= size_of::<T>() * 8 <= 32
+    ///   
+    /// # Examples  
+    /// ```
+    /// # use nbits::Iterator;
+    /// assert_eq!(
+    ///     vec![0b1111_1111, 0b1111_1111].bit_chunks(6).collect::<Vec<u8>>(),
+    ///     vec![0b11_1111, 0b11_1111, 0b11_1100]
+    /// );
+    /// assert_eq!(
+    ///     vec![0b1111_1111; 3].bit_chunks(11).collect::<Vec<u16>>(),
+    ///     vec![0b111_1111_1111, 0b111_1111_1111, 0b110_0000_0000]
+    /// );
+    /// ```
     fn bit_chunks<T>(&self, n: usize) -> impl Iterator<Item = T>
     where
         T: TryFrom<u64> + Default;
 
-    fn bit_from_chunk<T>(&mut self, n: usize, chunks: impl Iterator<Item = T>)
+    fn bit_from_chunks<T>(&mut self, n: usize, chunks: impl Iterator<Item = T>)
     where
         T: TryInto<u64>;
 }
@@ -48,14 +69,32 @@ impl BitIterator for [u8] {
         });
     }
 
-    fn bit_chunks<T>(&self, _n: usize) -> impl Iterator<Item = T>
+    fn bit_chunks<T>(&self, n: usize) -> impl Iterator<Item = T>
     where
         T: TryFrom<u64> + Default,
     {
-        self.iter().take(0).map(|_| T::default())
+        let valid_size = (std::mem::size_of::<T>() * 8).min(32);
+        assert_overflow!(n, 1, valid_size, "Chunks");
+
+        // enumerate bytes window of 64 bits width, split item values from those windows
+        let bit_mask: u64 = (0..n).fold(0, |acc, v| acc | (1 << v));
+        let mut bit_pos = 0;
+        (0..self.len()).flat_map(move |i| {
+            let window_value = self.byte_window_64(i);
+            let window_end = i * 8 + u64::BITS as usize; // current bit window end
+            debug_assert!(matches!(window_end - bit_pos, 1..=64)); // current bit window size
+
+            let mut vs = vec![];
+            while (bit_pos + n) <= window_end && (bit_pos + n) < self.len() * 8 + n {
+                bit_pos += n;
+                let value = (window_value >> (window_end - bit_pos)) & bit_mask;
+                vs.push(value.try_into().unwrap_or_default());
+            }
+            vs
+        })
     }
 
-    fn bit_from_chunk<T>(&mut self, _n: usize, _chunks: impl Iterator<Item = T>)
+    fn bit_from_chunks<T>(&mut self, _n: usize, _chunks: impl Iterator<Item = T>)
     where
         T: TryInto<u64>,
     {
@@ -63,58 +102,134 @@ impl BitIterator for [u8] {
     }
 }
 
-// pub trait FromBits {
-//     fn from_bits<U>(bits: U) -> Vec<u8>
-//     where
-//         U: Iterator<Item = bool>;
+pub trait FromBits {
+    /// Convert enumerated bool values to buffer
+    /// # Examples
+    /// ```
+    /// # use nbits::FromBits;
+    /// let bits = vec![false, false, false, false, true, true, true, true, false];
+    /// let data = Vec::from_bits(bits.into_iter());
+    /// assert_eq!(data, [0b0000_1111, 0b0000_0000]);
+    /// ```
+    fn from_bits<U>(bits: U) -> Vec<u8>
+    where
+        U: Iterator<Item = bool>;
 
-//     fn from_chunks<T, U>(chunks: U, n: usize) -> Vec<u8>
-//     where
-//         T: TryInto<u64>,
-//         U: Iterator<Item = T>;
-// }
+    /// Conjoin the lowest n bits of each value  
+    ///
+    /// # Parameters
+    /// - `n`: the number of bits to conjoin
+    /// - 1 <= n <= 32
+    /// - if n > size_of::<T>() * 8, left padding zero bits
+    ///
+    /// # Examples
+    /// ```
+    /// # use nbits::FromBits;
+    /// assert_eq!(
+    ///     Vec::from_chunks([0b11_1111_u8, 0b11_1111, 0b11_1111].into_iter(),6),
+    ///     vec![0b1111_1111, 0b1111_1111, 0b1100_0000]
+    /// );
+    /// assert_eq!(
+    ///     Vec::from_chunks([0b1111_u16, 0b1111, 0b1111].into_iter(), 6),
+    ///     vec![0b001111_00, 0b1111_0011, 0b1100_0000]
+    /// );
+    /// ```
+    fn from_chunks<T, U>(chunks: U, n: usize) -> Vec<u8>
+    where
+        T: TryInto<u64>,
+        U: Iterator<Item = T>;
+}
 
-// impl FromBits for Vec<u8> {
-//     fn from_bits<U>(_bits: U) -> Vec<u8>
-//     where
-//         U: Iterator<Item = bool>,
-//     {
-//         todo!()
-//     }
+impl FromBits for Vec<u8> {
+    fn from_bits<U>(bits: U) -> Vec<u8>
+    where
+        U: Iterator<Item = bool>,
+    {
+        let mut v = 0_u8;
+        bits.chain([false; 7])
+            .enumerate()
+            .filter_map(|(i, bit)| {
+                let n = 7 - (i % 8);
+                if bit {
+                    v |= 1 << n;
+                }
+                match n {
+                    0 => Some(std::mem::take(&mut v)),
+                    _ => None,
+                }
+            })
+            .collect()
+    }
 
-//     fn from_chunks<T, U>(_chunks: U, _n: usize) -> Vec<u8>
-//     where
-//         T: TryInto<u64>,
-//         U: Iterator<Item = T>,
-//     {
-//         todo!()
-//     }
-// }
+    fn from_chunks<T, U>(chunks: U, n: usize) -> Vec<u8>
+    where
+        T: TryInto<u64>,
+        U: Iterator<Item = T>,
+    {
+        assert_overflow!(n, 1, 32, "from_chunks");
+        let bit_mask: u64 = (0..n).fold(0, |acc, v| acc | (1 << v));
 
-// impl<const N: usize> FromBits for [u8; N] {
-//     fn from_bits<U>(_bits: U) -> Vec<u8>
-//     where
-//         U: Iterator<Item = bool>,
-//     {
-//         todo!()
-//     }
+        let mut rem = TinyBits::new(0, 0);
+        let mut vs: Vec<u8> = chunks
+            .map(|v| v.try_into().unwrap_or_default())
+            .flat_map(|mut value: u64| {
+                value &= bit_mask;
+                value <<= u64::BITS as usize - n;
+                rem.prefix_to(&mut value);
 
-//     fn from_chunks<T, U>(_chunks: U, _n: usize) -> Vec<u8>
-//     where
-//         T: TryInto<u64>,
-//         U: Iterator<Item = T>,
-//     {
-//         todo!()
-//     }
-// }
+                let partial = (n + rem.len()) / 8;
+                let bytes = value.to_be_bytes();
+                rem = TinyBits::new(bytes[partial], (n + rem.len()) % 8);
+                bytes[..partial].to_vec()
+            })
+            .collect();
+        vs.extend_from_slice(&rem.value());
+        vs
+    }
+}
 
-#[cfg(test)]
-mod test_iter {
-    use super::*;
+trait ByteWindow {
+    // get window value from bytes. If insufficient, tail padding zero
+    fn byte_window_64(&self, byte_index: usize) -> u64;
+}
 
-    #[test]
-    fn test_chunks() {
-        // Vec::from_chunks(chunks, n)
-        //        <[u8; 8]>::from_chunks(vec![0b1111_0000_u8, 0b1111_0000_u8].into_iter(), 8);
+impl ByteWindow for [u8] {
+    fn byte_window_64(&self, i: usize) -> u64 {
+        let bytes = self[i..]
+            .iter()
+            .copied()
+            .chain(std::iter::repeat(0))
+            .take(8)
+            .collect::<Vec<u8>>();
+        u64::from_be_bytes(bytes.try_into().unwrap())
+    }
+}
+
+/// 0~7 bits data
+#[derive(Debug)]
+struct TinyBits {
+    data: u8,
+    len: usize,
+}
+
+impl TinyBits {
+    pub fn new(data: u8, len: usize) -> Self {
+        TinyBits { data, len }
+    }
+    pub fn len(&self) -> usize {
+        self.len
+    }
+    pub fn value(&self) -> Vec<u8> {
+        match self.len > 0 {
+            true => vec![self.data],
+            false => vec![],
+        }
+    }
+    /// insert bits as value prefix
+    pub fn prefix_to(&self, value: &mut u64) {
+        if self.len > 0 {
+            *value >>= self.len;
+            *value |= (self.data as u64) << (64 - 8); // u64 left byte
+        }
     }
 }
--- src/xbits/mod.rs
+++ src/xbits/mod.rs
@@ -4,4 +4,4 @@ mod iter;
 
 pub use arith::Arithmetic;
 pub use bitwise::Bitwise;
-pub use iter::BitIterator as Iterator;
+pub use iter::{BitIterator as Iterator, FromBits};
